8b0536c9a03d404f5c34cff961a08d9401a827d0
Curtis <c.b.punches@gmail.com>
2021-04-27 07:53:55 -0400

    css

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
-fetch("https://www.reddit.com/r/aww/.json")
-.then(response => response.json())
-.then(data => {
+fetch('https://www.reddit.com/r/aww/.json')
+  .then((response) => response.json())
+  .then((data) => {
+    for (let kid of data.data.children) {
+      let mainArea = document.querySelector('main');
+      let newDiv = document.createElement('div');
+      let newImage = document.createElement('img');
+      let newLinks = document.createElement('a');
+      let newPara = document.createElement("p");
+      
+      let image = kid.data.thumbnail;
+      newImage.src = image;
+      
+      let title = kid.data.title;
+      newDiv.innerHTML = title;
+
+      let link = kid.data.url_overridden_by_dest;
+      newLinks.href = link;
+      newLinks.innerHTML = link;
+
+      let upVotes = kid.data.ups;
+     
+     newPara.innerText = upVotes;
 
 
-for (let kid of data.data.children){
- 
+  
 
-  let mainArea = document.querySelector("main");
-let newDiv = document.createElement("div");
-let newImage = document.createElement("img");
-let newLinks = document.createElement("a")
-let image = kid.data.thumbnail; 
-newImage.src = image;
-  let title = kid.data.title;
-
- let link = kid.data.url_overridden_by_dest;
- newLinks.href = link;
-
- newLinks.innerHTML=link;
-
-console.log(link)
-
- console.log(title);
- console.log(image);
- console.log(link);
+      mainArea.appendChild(newDiv);
+      newDiv.insertAdjacentElement('afterbegin', newPara);
+      newDiv.insertAdjacentElement('afterbegin', newImage);
+      newDiv.insertAdjacentElement('beforeend', newLinks);
 
 
-newDiv.innerHTML = title;
+      
 
-mainArea.appendChild(newDiv);
+      // TypeError: defaultDiv is not iterable, why???
+      // let defaultDiv = document.querySelector("main");
+      // for (let div of defaultDiv){
+      //   console.log(title);
+      // }
 
-// mainArea.appendChild(newLinks);
+      //  let defaultDiv = document.querySelectorAll("div");
+      //  console.log(defaultDiv);
+      // for (let divDelete of defaultDiv){
+      //   console.log(divDelete);}
+      //   //weird iteration!!!!!!
 
-newDiv.insertAdjacentElement("afterbegin",newImage);
-newDiv.insertAdjacentElement("beforeend", newLinks);
+      // if(divDelete.contains("default"){
+      //NEED TO ASK ON THIS ONE!!!!!!!!!!!!
+      //     console.log("red");
+      //   }
+      // }
 
-console.log(link);
-
-
-
-
-
-// let insideDiv = mainArea.querySelectorAll("div");
-// insideDiv.appendChild(newImage);
-// let imageArea = mainArea.querySelector("div");
-// imageArea.innerText = image;
-// let linkArea = mainArea.querySelector("div");
-// linkArea.innerText = link;
-// mainArea.appendChild(newImage);
-// mainArea.appendChild(newLinks);
-
-
-}
-
-
-
-
-});
+      // let insideDiv = mainArea.querySelectorAll("div");
+      // insideDiv.appendChild(newImage);
+      // let imageArea = mainArea.querySelector("div");
+      // imageArea.innerText = image;
+      // let linkArea = mainArea.querySelector("div");
+      // linkArea.innerText = link;
+      // mainArea.appendChild(newImage);
+      // mainArea.appendChild(newLinks);
+    }
+  });
